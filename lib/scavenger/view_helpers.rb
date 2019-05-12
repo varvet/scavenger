f56053376556cf88cb4c5af2041e5663f296beea
Matt Powell <fauxparse@gmail.com>
2019-05-13 10:23:45 +1200

    support '/' for nesting in view helper
    Translate a '/' character in the argument to the `svg` view helper
    into the `--` in the symbol name/class.
    
    Also update integration tests to reflect this change.

--- lib/scavenger/view_helpers.rb
+++ lib/scavenger/view_helpers.rb
@@ -9,12 +9,20 @@ module Scavenger
     end
 
     def svg(ref, options = {})
-      options[:class] = "#{Scavenger::Config.class_prefix}#{ref}" if options[:class].nil?
-      content_tag :svg, "<use xlink:href=\"##{Scavenger::Config.id_prefix}#{ref}\"/>".html_safe, options
+      options[:class] = scavenger_symbol_class(ref) if options[:class].nil?
+      content_tag :svg, "<use xlink:href=\"##{scavenger_symbol_ref(ref)}\"/>".html_safe, options
     end
 
     def scavenger_sprite_path
       asset_path File.basename(Scavenger::Config.sprite_path)
+    end
+
+    def scavenger_symbol_class(ref)
+      "#{Scavenger::Config.class_prefix}#{ref.gsub('/', '--')}"
+    end
+
+    def scavenger_symbol_ref(ref)
+      "#{Scavenger::Config.id_prefix}#{ref.gsub('/', '--')}"
     end
   end
 end
